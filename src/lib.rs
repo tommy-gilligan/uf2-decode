f84ec7428aedc8b2d2916b8ef51644740179c6bb
Thomas Gilligan <thomas.gilligan@icloud.com>
2023-09-15 20:24:36 +1000

    Make test slightly nicer

--- src/lib.rs
+++ src/lib.rs
@@ -66,7 +66,9 @@ pub fn convert_from_uf2(buf: &[u8]) -> (Vec<u8>, HashMap<u32, usize>) {
 
 #[cfg(test)]
 mod test {
-    use indoc::indoc;
+    use super::*;
+    use std::collections::HashMap;
+    use std::fs::read;
     use std::process::Command;
 
     #[test]
@@ -81,22 +83,22 @@ mod test {
             .output()
             .unwrap();
         assert_eq!(
-            indoc! {"
-                --- UF2 File Header Info ---
-                Family ID is RP2040, hex value is 0xe48bff56
-                Target Address is 0x10000000
-                All block flag values consistent, 0x2000
-                ----------------------------
-                Converted to bin, output size: 16640, start address: 0x10000000
-                Wrote 16640 bytes to ../../test.bin
-            "},
+            concat!(
+                "--- UF2 File Header Info ---\n",
+                "Family ID is RP2040, hex value is 0xe48bff56\n",
+                "Target Address is 0x10000000\n",
+                "All block flag values consistent, 0x2000\n",
+                "----------------------------\n",
+                "Converted to bin, output size: 16640, start address: 0x10000000\n",
+                "Wrote 16640 bytes to ../../test.bin\n"
+            ),
             String::from_utf8(output.stdout).unwrap()
         );
-        let (converted, family_to_target) = crate::convert_from_uf2(uf2);
+        let (converted, family_to_target) = convert_from_uf2(uf2);
         assert_eq!(
             family_to_target,
-            std::collections::HashMap::from([(0xe48b_ff56, 0x1000_0000)])
+            HashMap::from([(0xe48b_ff56, 0x1000_0000)])
         );
-        assert_eq!(converted, std::fs::read("test.bin").unwrap());
+        assert_eq!(converted, read("test.bin").unwrap());
     }
 }
