afbb7daf89d8ac1c3fa7c5f00e16f78a7402ca1d
Thomas Gilligan <thomas.gilligan@icloud.com>
2023-09-15 20:14:13 +1000

    Add some more metadata

---
+++ README.md
@@ -0,0 +1,5 @@
+# UF2
+
+Basic decoding of UF2.  This is a partial
+adaptation of
+[`uf2conv.py`](https://github.com/microsoft/uf2/blob/17e70bf908e6abdf4f4acc50a9c84e5709ded2a9/utils/uf2conv.py).
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
-//! Converts binary files from Microsoft's UF2 format
+//! # UF2
+//!
+//! Basic decoding of UF2.  This is a partial
+//! adaptation of
+//! [`uf2conv.py`](https://github.com/microsoft/uf2/blob/17e70bf908e6abdf4f4acc50a9c84e5709ded2a9/utils/uf2conv.py).
 
 use std::collections::HashMap;
 
